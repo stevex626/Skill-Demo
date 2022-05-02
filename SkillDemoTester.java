5b67738b37318390be7bab9e9c8323d756e047f0
stevex626 <97060332+stevex626@users.noreply.github.com>
2022-05-01 21:08:00 -0700

    Update SkillDemoTester.java

--- SkillDemoTester.java
+++ SkillDemoTester.java
@@ -3,6 +3,6 @@ import org.junit.*;
 public class SkillDemoTester {
     @Test
     public void test(){
-        assertEquals("even", SkillDemo.oddOrEven(1, 4)); // failing test
+        assertEquals("odd", SkillDemo.oddOrEven(1, 4)); // failing test
     }
 }
